3d351d6e630300f08651fc5a0c26879af201d48b
22091397034_Khofifah Wulandari <124429174+22091397034-KhofifahWulandari@users.noreply.github.com>
2023-10-13 14:30:11 +0700

    Update galleryscript.js

--- galleryscript.js
+++ galleryscript.js
@@ -9,3 +9,10 @@
 
  // Loop through the "stars" NodeList Again
  stars.forEach((star, index2) => {
+
+ // Add the "active" class to the clicked star and any stars with a lower index
+ // and remove the "active" class from any stars with a higher index
+ index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+});
+});
+});
